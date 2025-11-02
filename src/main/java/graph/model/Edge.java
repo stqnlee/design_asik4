f27f8b139e9f99ce8ab63e5057a22bfae3b49ea5
stqnlee <nuryshevkaisar@gmail.com>
2025-11-02 21:00:11 +0500

    Add Edge class with vertices and weight

---
+++ src/main/java/graph/model/Edge.java
@@ -0,0 +1,7 @@
+package graph.model;
+
+public final class Edge {
+    public final int u, v;
+    public final long w;
+    public Edge(int u, int v, long w) { this.u = u; this.v = v; this.w = w; }
+}
